2a44e9e337a343f43ee9ec0aaa08b0d2f3439609
Daisy Yi <daisyyi@rocketmail.com>
2024-04-09 12:09:04 -0700

    pass second test

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -6,8 +6,15 @@ def admin_login(username, password):
     return "Access denied"
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 49:
+        return "It's brisk out there!"
+    elif temperature > 40 and temperature < 64:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
+
 
 def fizzbuzz(num):
     # your code here
