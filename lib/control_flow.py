3d867d7e4cb5efcdabfa949db851ce9a59d3a8ab
Daisy Yi <daisyyi@rocketmail.com>
2025-01-19 15:57:23 -0700

    problem 2 solved

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -6,7 +6,15 @@ def admin_login(username, password):
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif 40 <= temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
+
 
 def fizzbuzz(num):
     # your code here
