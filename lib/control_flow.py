d3a95fa6a4d5e72ab286a2672e5d80066f771300
Daisy Yi <daisyyi@rocketmail.com>
2025-01-19 16:03:22 -0700

    problem 3 solved

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -8,7 +8,7 @@ def hows_the_weather(temperature):
     # your code here
     if temperature < 40:
         return "It's brisk out there!"
-    elif 40 <= temperature <= 65:
+    elif 40 <= temperature < 65:
         return "It's a little chilly out there!"
     elif temperature > 85:
         return "It's too dang hot out there!"
@@ -18,7 +18,14 @@ def hows_the_weather(temperature):
 
 def fizzbuzz(num):
     # your code here
-    pass
+    if num % 3 == 0 and num %5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
 def calculator(operation, num1, num2):
     # your code here
